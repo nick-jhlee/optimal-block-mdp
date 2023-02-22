16ae557711f85698bf7bf322e95aaa16c53cbcf7
Junghyun Lee (Macbook-Pro) <nick.jhlee00@gmail.com>
2023-02-22 21:23:16 +0900

    Code refactoring, change BMDP instance, implement parallel loops

--- Experiment.py
+++ Experiment.py
@@ -1,46 +1,88 @@
 from Clustering import *
+
+import multiprocess
+from multiprocess import Pool
+import time
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def simulate(env, T, num_repeats, delta1=0, delta2=0, delta3=0):
-    init_errors, final_errors = [], []
-    for _ in range(num_repeats):
-        # obtain (possibly corrupted) trajectories
-        trajectories = corrupt(env, generate_trajectories(T, env), delta1=delta1, delta2=delta2, delta3=delta3)
-        # obtain transition matrices
-        transition_matrices = empirical_transitions(env, trajectories)
+def simulate_single_run(env, T, delta1=0, delta2=0, delta3=0):
+    # obtain (possibly corrupted) trajectories
+    trajectories = corrupt(env, generate_trajectories(T, env), delta1=delta1, delta2=delta2, delta3=delta3)
+    # obtain transition matrices
+    transition_matrices = empirical_transitions(env, trajectories)
 
-        # initial spectral clustering
-        f_1 = init_spectral(env, T, transition_matrices)
-        init_err_rate = error_rate(env, f_1)
-        init_errors.append(init_err_rate)
-        # print("Error rate after initial clustering is ", init_err_rate)
+    # initial spectral clustering
+    f_1 = init_spectral(env, T, transition_matrices)
+    init_err_rate = error_rate(env, f_1)
+    # print("Error rate after initial clustering is ", init_err_rate)
 
-        # likelihood_improvement
-        # f_final, errors = likelihood_improvement(env, trajectories, f_1, num_iter=10)
-        f_final, errors = likelihood_improvement(env, transition_matrices, f_1, num_iter=None)
-        final_errors.append(errors[-1])
-        # print("Final error rate is ", errors[-1])
-        # print("Errors along the improvement steps: ", errors)
-    return init_errors, final_errors
+    # likelihood_improvement
+    # f_final, errors = likelihood_improvement(env, trajectories, f_1, num_iter=10)
+    f_final, errors = likelihood_improvement(env, transition_matrices, f_1, num_iter=None)
+
+    # return init and final errors
+    return init_err_rate, errors[-1]
 
 
-def plot(dfs, id_var, exp_num, title_str):
+# source: https://stackoverflow.com/questions/19562916/print-progress-of-pool-map-async
+def track_job(job, exp_description, update_interval=3):
+    while job._number_left > 0:
+        tmp = str(job._number_left * job._chunksize).zfill(4)
+        print(f"({exp_description}) Tasks remaining = {tmp}", end='\r')
+        time.sleep(update_interval)
+
+
+def simulate(env, T, num_repeats, exp_description, delta1=0, delta2=0, delta3=0):
+    def F(repeat):
+        result = simulate_single_run(env, T, delta1, delta2, delta3)
+
+        # # print process id
+        # print(f"Finished: {multiprocess.process.current_process()}, TH = {T * env.H}")
+        return result
+
+    repeats = list(range(num_repeats))
+    # run the experiments in parallel
+    with Pool() as pool:
+        everything = pool.map_async(F, repeats)
+        track_job(everything, exp_description)
+        everything = everything.get()
+
+    tmp = list(zip(*everything))
+    return list(tmp[0]), list(tmp[1])
+
+
+def plot_final_discrete(final_means, final_stds, xs, title, xlabel, legends, fname):
+    fig, ax = plt.subplots()
+    clrs = sns.color_palette("husl", len(legends))
+
+    with sns.axes_style("darkgrid"):
+        for i, color in enumerate(clrs):
+            ax.errorbar(xs, final_means[i], yerr=final_stds[i], fmt='o', linestyle='dashed', capsize=3,
+                        label=legends[i], c=color)
+
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    # plt.savefig(fname, dpi=1200, bbox_inches='tight')
+    plt.show()
+    plt.close()
+
+
+def plot(dfs, exp_num, title_str):
     # plot and save
     cdf = pd.concat(dfs)
-    mdf = pd.melt(cdf, id_vars=[id_var], var_name="Algorithm", value_name="error rate")
+    mdf = pd.melt(cdf, id_vars=["param"], var_name="Algorithm", value_name="error rate")
 
     fig, ax = plt.subplots()
-    sns.boxplot(x=id_var, y="error rate", hue="Algorithm", data=mdf, ax=ax)
+    sns.boxplot(x="param", y="error rate", hue="Algorithm", data=mdf, ax=ax, whis=1.1)
     # sns.stripplot(x="H", y="error rate", hue="Algorithm", data=mdf, dodge=True, ax=ax, legend=False)
     fig.suptitle(title_str)
     plt.savefig(f"results/plot_exp{exp_num}.pdf", dpi=500)
 
-    plt.show()
+    # plt.show()
     plt.clf()
     plt.close()
-
-    cp.savez_compressed(f"raw_datas/exp{exp_num}", dfs=dfs)
-    return None
---
+++ main.py
@@ -0,0 +1,108 @@
+"""
+Created on 2023.02.22
+@author: nicklee
+
+(Description)
+"""
+from Experiment import *
+
+
+def main(S, A, n, num_repeats, param_range, param_name):
+    dfs = []
+    delta1, delta2, delta3 = 0, 0, 0
+    for i, param in enumerate(param_range):
+        if param_name == 'eta':
+            eta = round(param, 2)
+            H = n
+            T = 20
+
+            exp_num = 3
+        else:
+            # symmetric binary case
+            eta = 2.00
+            if param_name == 'T':
+                T = param
+                H = n
+
+                exp_num = 1
+            elif param_name == 'H':
+                H = param
+                T = 20
+
+                exp_num = 2
+            elif 'delta' in param_name:
+                eta = 2.0
+                H = n
+                T = 20
+
+                exp_num = 3 + int(param_name[-1])
+
+                if exp_num == 4:
+                    delta1 = param
+                    delta2, delta3 = 0, 0
+                elif exp_num == 5:
+                    delta2 = param
+                    delta1, delta3 = 0, 0
+                elif exp_num == 6:
+                    delta3 = param
+                    delta1, delta2 = 0, 0
+                else:
+                    raise NotImplementedError(f"Experiment {exp_num} not implemented!")
+            else:
+                raise NotImplementedError(f"{param_name} not implemented!")
+
+        # symmetric binary case
+        latent_transitions = [cp.array([[eta / (1 + eta), 1 / (1 + eta)], [1 / (1 + eta), eta / (1 + eta)]]),
+                              cp.array([[1 / (1 + eta), eta / (1 + eta)], [eta / (1 + eta), 1 / (1 + eta)]]),
+                              cp.array([[1 / 2, 1 / 2], [1 / 2, 1 / 2]])]
+        # set environment
+        env_config = {'n': n, 'H': H, 'S:': S, 'A': A,
+                      'ps': latent_transitions,
+                      'qs': 'uniform'}
+        env = Synthetic(env_config)
+
+        init_errors, final_errors = simulate(env, T, num_repeats, f"{param_name} = {param}", delta1, delta2, delta3)
+        if verbose:
+            print("initial: ", init_errors)
+            print("final: ", final_errors)
+        # logging
+        dfs.append(pd.DataFrame(cp.array([init_errors, final_errors]).T,
+                                columns=["Init Spectral", "Likelihood Improvement"]).assign(param=param))
+
+    # save raw data
+    cp.savez_compressed(f"raw_datas/exp{exp_num}", dfs=dfs)
+    # plot
+    plot(dfs, exp_num, f"Varying {param_name}")
+
+
+if __name__ == '__main__':
+    num_repeats = 1000
+    verbose = False
+
+    # number of actions, clusters
+    S, A = 2, 3
+    n = 100
+
+    # Experiment 1. Varying T
+    T_range = range(5, 50, 5)
+    main(S, A, n, num_repeats, T_range, 'T')
+
+    # Experiment 2. Varying H
+    H_range = range(int(n / 5), n + 10, 10)
+    main(S, A, n, num_repeats, H_range, 'H')
+    
+    # Experiment 3. Varying eta
+    eta_range = [1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0]
+    main(S, A, n, num_repeats, eta_range, 'eta')
+
+    # Experiment 4. Varying delta1
+    delta1_range = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    main(S, A, n, num_repeats, delta1_range, 'delta1')
+
+    # Experiment 5. Varying delta2
+    delta2_range = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    main(S, A, n, num_repeats, delta2_range, 'delta1')
+
+    # Experiment 6. Varying delta3
+    delta3_range = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    main(S, A, n, num_repeats, delta3_range, 'delta1')
